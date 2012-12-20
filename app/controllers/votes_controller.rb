04e2d07cd8d9e71783ac9431c7c010a310f81668
Amy Lai <amy.jh.lai@gmail.com>
2012-12-19 22:35:53 -0500

    fix user show bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,13 @@
 class VotesController < ApplicationController
 
   def create
+    word = Word.find(params[:word_id])
   	mnemonic = Mnemonic.find(params[:mnemonic_id])
   	vote = Vote.new(params[:vote])
   	vote.mnemonic = mnemonic
   	vote.user = current_user
   	vote.save
-    redirect_to word_path(mnemonic.word)
+    redirect_to word_path(word)
   end
 
 end
